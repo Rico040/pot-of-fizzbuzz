d23f47594e1e2f2b55e9cc7752283c7380c4d97a
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-08-09 11:47:24 +0900

    Pascal: sane

--- Pascal.pas
+++ Pascal.pas
@@ -7,12 +7,10 @@ begin
     for i := 1 to 25000 do
         if i mod 15 = 0 then
             writeln('FizzBuzz')
-        else
-            if i mod 3 = 0 then
-                writeln('Fizz')
-            else
-                if i mod 5 = 0 then
-                    writeln('Buzz')
-                else
-                    writeln(i);
+        else if i mod 3 = 0 then
+            writeln('Fizz')
+        else if i mod 5 = 0 then
+            writeln('Buzz')
+        elsw
+            writeln(i);
 end.
