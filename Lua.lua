ee65a1625293ac8db6bbf70424f77ec2a6f8b992
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-08-09 11:49:05 +0900

    Lua: use else if which i haven't used before cuz i was stupid af

--- Lua.lua
+++ Lua.lua
@@ -1,16 +1,11 @@
 for i = 1, 25000, 1 do
     if i % 15 == 0 then
         print("FuzzBizz")
-        goto continue
-    end 
-    if i % 3 == 0 then
+    elseif i % 3 == 0 then
         print("Fizz")
-        goto continue
-    end
-    if i % 5 == 0 then
+    elseif i % 5 == 0 then
         print("Buzz")
-        goto continue
+    else
+        print(i)
     end
-    print(i)
-    ::continue::
 end
