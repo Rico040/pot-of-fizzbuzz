587c1a42f46ef9b2d765eba23a196fbec09c1d47
Rico040 <93081680+Rico040@users.noreply.github.com>
2026-07-05 19:18:38 +1000

    F#

---
+++ FSharp.fs
@@ -0,0 +1,8 @@
+for i in 1..25000 do
+     if i % 15 = 0 then
+         printfn "FizzBuzz"
+     elif i % 3 = 0 then
+         printfn "Fizz"
+     elif i % 5 = 0 then
+         printfn "Buzz"
+     else printfn $"%i{i}"
